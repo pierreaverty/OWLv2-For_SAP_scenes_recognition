9eb5bf12c6bb47968b8ec5d37e8d715fd8721692
pierreaverty <pierre.averty2001@gmail.com>
2024-02-23 13:00:48 +0100

    Addind specifications in sap_scenes dataset by adding a subclass SAPWomenDetectionDataset.

--- owlv2-for_sap_scenes_recognition/datasets/sap_scenes.py
+++ owlv2-for_sap_scenes_recognition/datasets/sap_scenes.py
@@ -1,3 +1,5 @@
+
+
 import os
 from PIL import Image
 from torch.utils.data import Dataset
@@ -21,10 +23,9 @@ class SAPDetectionDataset(Dataset):
         __len__(): Returns the length of the dataset.
         __getitem__(idx): Returns the item at the given index.
         _create_dataset(): Creates the dataset by loading images and labels.
-
     """
 
-    def __init__(self, images_directory, labels_directory, processor):
+    def __init__(self, images_directory, labels_directory, processor=None):
         self.images_directory = images_directory
         self.labels_directory = labels_directory
         self.images_files = os.listdir(images_directory)
@@ -56,10 +57,42 @@ class SAPDetectionDataset(Dataset):
                     "area": [0]
                 }
             })
-            
         return dataset
 
-# Usage example:
-images_directory = "./data/images/"
-labels_directory = "./data/labels/"
-dataset = SAPDetectionDataset(images_directory, labels_directory)
+class SAPWomenDetectionDataset(SAPDetectionDataset):
+    """
+    A custom dataset class for SAP women detection, inheriting from SAPDetectionDataset.
+
+    Args:
+        images_directory (str): The directory path containing the images.
+        labels_directory (str): The directory path containing the labels.
+        processor (optional): The processor to apply to the dataset.
+
+    Methods:
+        __init__(images_directory, labels_directory, processor): Initializes the SAPWomenDetectionDataset.
+        _create_dataset(): Creates the dataset by loading images and labels, filtering for images with "woman" in the file name.
+    """
+
+    def __init__(self, images_directory, labels_directory, processor=None):
+        super().__init__(images_directory, labels_directory, processor)
+
+    def _create_dataset(self):
+        dataset = []
+        for i, image_file in enumerate(self.images_files):
+            if "woman" in image_file:
+                image = Image.open(os.path.join(self.images_directory, image_file))
+                label_file = open(os.path.join(self.labels_directory, image_file.split(".")[0] + ".txt"), "r")
+                label_content = label_file.read().splitlines()[0].split(" ")
+                label = [float(label) for label in label_content]
+                dataset.append({
+                    "image_id": i,
+                    "image": image,
+                    "width": image.width,
+                    "height": image.height,
+                    "object": {
+                        "bbox": [label[1:]],
+                        "label": [int(label[0])],
+                        "area": [0]
+                    }
+                })
+        return dataset
--- owlv2-for_sap_scenes_recognition/main.py
+++ owlv2-for_sap_scenes_recognition/main.py
@@ -8,6 +8,7 @@ import matplotlib.patches as patches
 
 from transformers import Owlv2Processor, Owlv2ForObjectDetection
 from utils.plotter import ObjectDetectionPlotter
+from datasets.sap_scenes import SAPWomenDetectionDataset
 from PIL import Image
 
 class OWLv2SceneRecognition:
@@ -20,8 +21,8 @@ class OWLv2SceneRecognition:
 
 def main():
     # Create an instance of the OWLv2SceneRecognition class
-    owl = OWLv2SceneRecognition()
-    
+    # owl = OWLv2SceneRecognition()
+    dataset = SAPWomenDetectionDataset(images_directory="/home/omilab-gpu/OWLv2-For_SAP_scenes_recognition/data/images", labels_directory="/home/omilab-gpu/OWLv2-For_SAP_scenes_recognition/data/labels")
     
 
 
